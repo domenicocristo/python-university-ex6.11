555f245498d03283aab5a158264962a35ef72189
Domenico Cristo <84397096+domenicocristo@users.noreply.github.com>
2023-02-10 00:43:49 +0100

    ex6.4

---
+++ ex6.4.py
@@ -0,0 +1,14 @@
+# Un numero, a, è una potenza di b se è divisibile per b e a/b è a sua volta una potenza di b.
+
+# Scrivete una funzione di nome potenza che prenda come parametri a e b e che restituisca True 
+# se a è una potenza di b. Nota: dovete pensare bene al caso base.
+
+def potenza(a, b):
+    if a == b:
+        return True
+    elif a < b or a % b != 0:
+        return False
+    else:
+        return potenza(a/b, b)
+
+print(potenza(16, 2))
