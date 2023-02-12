45fbfd2cd641fe1a9e8ba4372618d30a5e9feff6
Domenico Cristo <84397096+domenicocristo@users.noreply.github.com>
2023-02-12 01:08:53 +0100

    Update ex6.5.py

--- ex6.5.py
+++ ex6.5.py
@@ -1,10 +1,12 @@
-# Il massimo comun divisore (MCD) di due interi a e b è il numero intero più grande 
-# che divide entrambi senza dare resto.
+"""
+Il massimo comun divisore (MCD) di due interi a e b è il numero intero più grande 
+che divide entrambi senza dare resto.
 
-# Un modo per trovare il MCD di due numeri si basa sull'osservazione che, se r è il resto della divisione 
-# tra a e b, allora mcd(a,b) = mcd(b,r). Come caso base, possiamo usare mcd(a,0) = a.
+Un modo per trovare il MCD di due numeri si basa sull'osservazione che, se r è il resto della divisione 
+tra a e b, allora mcd(a,b) = mcd(b,r). Come caso base, possiamo usare mcd(a,0) = a.
 
-# Scrivete una funzione di nome mcd che abbia come parametri a e b e restituisca il loro massimo comun divisore.
+Scrivete una funzione di nome mcd che abbia come parametri a e b e restituisca il loro massimo comun divisore.
+"""
 
 def mcd(a, b):
     if b == 0:
