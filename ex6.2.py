20f67471703040cbdd4ebd06e432deb5d28be850
Domenico Cristo <84397096+domenicocristo@users.noreply.github.com>
2023-02-12 01:01:55 +0100

    Update ex6.2.py

--- ex6.2.py
+++ ex6.2.py
@@ -1,12 +1,14 @@
-# La funzione di Ackermann, A(m, n), è così definita:
-#           { n + 1                  se m = 0
-# A(m, n) = { A(m - 1, 1)            se m > 0 e n = 0
-#           { A(m - 1, A(m, n - 1))  se m > 0 e n > 0.
+"""
+La funzione di Ackermann, A(m, n), è così definita:
+          { n + 1                  se m = 0
+A(m, n) = { A(m - 1, 1)            se m > 0 e n = 0
+          { A(m - 1, A(m, n - 1))  se m > 0 e n > 0.
+          
+Scrivete una funzione di nome ack che valuti la funzione di Ackermann. Usate la vostra funzione per calcolare 
+ack(3, 4), vi dovrebbe risultare 125.
 
-# Scrivete una funzione di nome ack che valuti la funzione di Ackermann. Usate la vostra funzione per calcolare 
-# ack(3, 4), vi dovrebbe risultare 125.
-
-# Cosa succede per valori maggiori di m e n?
+Cosa succede per valori maggiori di m e n?
+"""
 
 def ack(m, n):
     if m == 0:
